e4d9e3bb5cacf5746963b34fe69e1a0b26ff32dd
CDBJS <codingbjs@gmail.com>
2022-02-13 09:35:20 +0900

    CrimeFragment CrimeActivity  commit

--- app/src/main/java/com/codingbjs/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/codingbjs/criminalintent/CrimeActivity.java
@@ -1,14 +1,31 @@
 package com.codingbjs.criminalintent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import com.codingbjs.criminalintent.databinding.ActivityCrimeBinding;
+import com.codingbjs.criminalintent.fragment.CrimeFragment;
+
 public class CrimeActivity extends AppCompatActivity {
+
+    ActivityCrimeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        binding = ActivityCrimeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null) {
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
